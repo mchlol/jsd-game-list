75424db91d43360f63e00ba81f6ecf94c815d3fe
Michelle Brown <m.andreabr@gmail.com>
2023-08-13 17:31:24 +1000

    try using fetch instead of axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 function App() {
@@ -8,50 +8,33 @@ function App() {
   const [games, setGames] = useState();
   const [loading, setLoading] = useState(false);
 
-  // useEffect( () => {
-  //   async function fetchGames() {
-  //     const dataSource = `/.netlify/functions/gamelist`;
-  //     try {
-  //       setLoading(true);
-
-  //       const gameList = await fetch(dataSource)
-  //         .then( (res) => res.json());
-  //         setGames(gameList);
-  //         console.log('games',games);
-  //         console.log('Type of games',typeof games)
-  //         console.log(games.response);
-
-  //     } catch (err) {
-  //       console.log('error:', err);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-  //   fetchGames();
-  // }, []);
-
-
   useEffect( () => {
     async function fetchGames() {
-      axios.get(`https://rawg.io/api/games?token&key=${process.env.REACT_APP_API_KEY}`)
-      .then ( res => {
-        console.log('Response:',res.data.results);
-        setGames(res.data.results);
-      })
-      .then ( setLoading(false))
-      .catch ( err => {
-        console.log('Error:',err);
-      })
+      const dataSource = `https://rawg.io/api/games?token&key=${process.env.REACT_APP_API_KEY}`;
+      try {
+        setLoading(true);
+
+        const gameList = await fetch(dataSource)
+          .then( (res) => res.json());
+          setGames(gameList);
+          console.log('games',games);
+          console.log('Type of games',typeof games)
+          console.log(games.response);
+          console.log('gameList:',gameList);
+
+      } catch (err) {
+        console.log('error:', err);
+      } finally {
+        setLoading(false);
+      }
     }
     fetchGames();
   }, []);
 
-
-
   return (
     <div className="App">
       <p>Check console 🧑‍💻</p>
-      <p> {loading ? "Loading..." : games.map (game => game.name) } </p>
+      <p> {loading ? "Loading..." : 'Content loaded' } </p>
     </div>
   );
 }
