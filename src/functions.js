e9301249d8185470d8fac532797bfec7a7f332d1
Michelle Brown <m.andreabr@gmail.com>
2023-08-15 12:39:13 +1000

    create a file to store any funtions that need to be reused across components

---
+++ src/functions.js
@@ -0,0 +1,57 @@
+const formatDate = function(date) {
+    if (date === null) {
+        return 'Unknown'
+    } else {
+        // date is either null or YYYY-MM-DD
+    const year = date.slice(0,4);
+    let month = date.slice(5,7);
+    const day = date.slice(8,10)
+
+    switch(month) {
+        case '01':
+            month = 'January';
+            break;
+        case '02':
+            month = 'February';
+            break;
+        case '03':
+            month = "March";
+            break;
+        case '04':
+            month = 'April';
+            break;
+        case '05':
+            month = 'May';
+            break;
+        case '06':
+            month = 'June';
+            break;
+        case '07':
+            month = 'July';
+            break;
+        case '08':
+            month = 'August';
+            break;
+        case '09':
+            month = 'September';
+            break;
+        case '10':
+            month = 'October';
+            break;
+        case '11':
+            month = 'November';
+            break;
+        case '12':
+            month = 'December';
+            break;
+        default:
+            console.log('Could not determine month');
+    }
+
+    let dateString = `${day} ${month} ${year}`;
+    return dateString;
+    }
+    
+}; // formatDate
+
+export { formatDate };
