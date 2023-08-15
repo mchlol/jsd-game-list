79e7b3390f907979f2acfe28ddce4d0f045e11cf
Michelle Brown <m.andreabr@gmail.com>
2023-08-15 12:40:46 +1000

    rearrange so next page button is part of the if loading false expression

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,56 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import { Loading, Card, Button } from "react-daisyui";
+import { formatDate } from "../functions";
 
-const formatDate = function(date) {
-    // date is always formatted YYYY-MM-DD
-    const year = date.slice(0,4);
-    let month = date.slice(5,7);
-    const day = date.slice(8,10)
-
-    switch(month) {
-        case '01':
-            month = 'January';
-            break;
-        case '02':
-            month = 'February';
-            break;
-        case '03':
-            month = "March";
-            break;
-        case '04':
-            month = 'April';
-            break;
-        case '05':
-            month = 'May';
-            break;
-        case '06':
-            month = 'June';
-            break;
-        case '07':
-            month = 'July';
-            break;
-        case '08':
-            month = 'August';
-            break;
-        case '09':
-            month = 'September';
-            break;
-        case '10':
-            month = 'October';
-            break;
-        case '11':
-            month = 'November';
-            break;
-        case '12':
-            month = 'December';
-            break;
-        default:
-            console.log('Could not determine month');
-    }
-    let dateString = `${day} ${month} ${year}`;
-    return dateString;
-}; // formatDate
 
 function SearchResults() {
 
@@ -66,7 +18,7 @@ function SearchResults() {
     useEffect( () => {
         console.log('useEffect callback running');
         loadSearchResults(params.query);
-    }, []);
+    }, [params.query]);
     
     function loadSearchResults (query) {
         setLoading(true);
@@ -90,34 +42,38 @@ function SearchResults() {
 
     return (
         <>
-        <div id="searchResults">
+        <div>
             {
                 loading
                 ?
                 <Loading />
                 :
-                
-                games.map( game =>
-                <Card 
-                className="max-w-md game-card"
-                onClick={ () => navigate(`/game/${game.slug}`)} 
-                key={game.slug} 
-                >
-                    <Card.Image className="gameImg" src={game.background_image} alt={game.name} />
-                    
-                    <Card.Body>
-                        <Card.Title tag="h2">{game.name}
-                    </Card.Title>
-                        <span className="spanFormat">Released ({formatDate(game.released)})</span>
-                        <Card.Actions className="justify-end">
-                            <Button className="btn btn-sm btn-primary">View</Button>
-                        </Card.Actions>
-                    </Card.Body>
-                </Card> 
-                )
+                <div id="searchResults">
+
+                    { games.map( game =>
+                    <Card 
+                    className="p-4 max-w-md game-card"
+                    onClick={ () => navigate(`/game/${game.slug}`)} 
+                    key={game.slug} 
+                    >
+                        <Card.Image className="gameImg" src={game.background_image} alt={game.name} />
+                        
+                        <Card.Body>
+                            <Card.Title tag="h2">{game.name}
+                        </Card.Title>
+                            <span className="spanFormat">Released {formatDate(game.released)}</span>
+                            <Card.Actions className="justify-end">
+                                <Button className="btn btn-sm btn-primary">View</Button>
+                            </Card.Actions>
+                        </Card.Body>
+                    </Card> 
+                    ) } 
+
+                    <Button>Next Page →</Button>
+                </div>
                 
             }
-            <Button>Next Page →</Button>
+            
         </div>
         
         </>
