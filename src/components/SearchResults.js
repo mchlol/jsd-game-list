89ebb401b74297d8aff330543ee03ae683259f13
Michelle Brown <m.andreabr@gmail.com>
2023-08-16 18:47:59 +1000

    begin working on pagination

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -15,10 +15,17 @@ function SearchResults() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const[page, setPage] = useState(1);
+
     useEffect( () => {
         console.log('useEffect callback running');
         loadSearchResults(params.query);
     }, [params.query]);
+
+    useEffect ( () => {
+        console.log('page useeffect callback');
+
+    },[page]);
     
     function loadSearchResults (query) {
         setLoading(true);
@@ -27,9 +34,9 @@ function SearchResults() {
 
         let url;
         if (query) {
-            url = `https://rawg.io/api/games?search=${query}&ordering=-rating&token&key=${process.env.REACT_APP_API_KEY}`;
+            url = `https://rawg.io/api/games?search=${query}&ordering=-rating&page=${page}&page_size=20&token&key=${process.env.REACT_APP_API_KEY}`;
         } else {
-            url = `https://rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`;
+            url = `https://rawg.io/api/games?page=${page}&page_size=20&key=${process.env.REACT_APP_API_KEY}`;
         }
 
         axios.get(url)
@@ -57,28 +64,44 @@ function SearchResults() {
                 ?
                 <Loading />
                 :
-                <div className="searchResults">
+                <div>
+                    <div className="searchResults">
 
-                    { games.map( game =>
-                    <Card 
-                    className="p-4 max-w-md game-card"
-                    onClick={ () => navigate(`/game/${game.slug}`)} 
-                    key={game.slug} 
-                    >
-                        <Card.Image className="gameImg" src={game.background_image} alt={game.name} />
-                        
-                        <Card.Body>
-                            <Card.Title tag="h2">{game.name}
-                        </Card.Title>
-                            <span className="spanFormat">Released {formatDate(game.released)}</span>
-                            <Card.Actions className="justify-end">
-                                <Button className="btn btn-sm btn-primary">View</Button>
-                            </Card.Actions>
-                        </Card.Body>
-                    </Card> 
-                    ) } 
+                        { games.map( game =>
+                        <Card 
+                        className="p-4 max-w-md game-card"
+                        onClick={ () => navigate(`/game/${game.slug}`)} 
+                        key={game.slug} 
+                        >
+                            <Card.Image className="gameImg" src={game.background_image} alt={game.name} />
+                            
+                            <Card.Body>
+                                <Card.Title tag="h2">{game.name}
+                            </Card.Title>
+                                <span className="spanFormat">Released {formatDate(game.released)}</span>
+                                <Card.Actions className="justify-end">
+                                    <Button className="btn btn-sm btn-primary">View</Button>
+                                </Card.Actions>
+                            </Card.Body>
+                        </Card> 
+                        ) } 
 
-                    <Button>Next Page →</Button>
+                    </div>
+
+                    <div className="p-4 flex justify-center">
+                        <Button 
+                        disabled={page === 1}
+                        onClick={ () => setPage( (prevState) => prevState -1)}
+                        >
+                            ← Previous
+                        </Button>
+                        <p>Page {page}</p>
+                        <Button
+                        onClick={() => setPage( (prevState) => prevState + 1)}
+                        >
+                            Next →
+                        </Button>
+                    </div>
                 </div>
                 
             }
